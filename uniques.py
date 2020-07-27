50c5a61aeb84dcd6c2442e69558358b9a9f47d2e
Aedan Cullen <aedan@rice.edu>
2020-07-26 22:33:31 -0500

    add ffmpeg static

--- uniques.py
+++ uniques.py
@@ -16,7 +16,7 @@ def process(song, performer):
     topurl = 'https://www.youtube.com' + res[0]['href']
 
     print(textToSearch, topurl)
-    os.system('youtube-dl -f bestaudio --output "out/out.%(ext)s" ' + topurl)
+    os.system('youtube-dl --extract-audio --audio-format wav --output "out/out.%(ext)s" --ffmpeg-location ffmpeg-4.3.1-amd64-static/ffmpeg ' + topurl)
 
 
 songids = []
