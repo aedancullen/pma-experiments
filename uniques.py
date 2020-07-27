9ba9c738606a4a8d004828c42b7695460f986ee7
Aedan Cullen <aedan@rice.edu>
2020-07-26 21:37:33 -0500

    Initial commit

---
+++ uniques.py
@@ -0,0 +1,31 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+
+def process(song, performer):
+    headers = {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
+
+    textToSearch = song + " " + performer
+    url = 'https://www.youtube.com/results'
+
+    response = requests.get(url, params={'search_query': textToSearch}, headers=headers)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    res = soup.findAll(attrs={'class':'yt-uix-tile-link'})
+    topurl = 'https://www.youtube.com' + res[0]['href']
+
+    print(textToSearch, topurl)
+
+
+songids = []
+
+with open("hs.csv", "r") as file:
+    csv = csv.reader(file, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
+    print(next(csv))
+    for line in csv:
+        url,weekid,week_position,song,performer,songid,instance,previous_week_position,peak_position,weeks_on_chart = tuple(line)
+        if not int(weekid[-4:]) >= 2010:
+            continue
+        if not songid in songids:
+            songids.append(songid)
+            process(song,performer)
