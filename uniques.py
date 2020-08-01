fb093d66586f14190e8198a6a580305d3dd5d6c0
Aedan Cullen <aedan@rice.edu>
2020-08-01 09:20:34 -0400

    duplicates, quotes

--- uniques.py
+++ uniques.py
@@ -9,16 +9,16 @@ import multiprocessing as mp
 import h5py
 
 def process(song, performer, year):
-    headers = {'User-Agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
+    headers = {"User-Agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"}
 
-    textToSearch = song + " " + performer
-    url = 'https://www.youtube.com/results'
+    textToSearch = song + ' ' + performer
+    url = "https://www.youtube.com/results"
 
-    response = requests.get(url, params={'search_query': textToSearch}, headers=headers)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    response = requests.get(url, params={"search_query": textToSearch}, headers=headers)
+    soup = BeautifulSoup(response.content, "html.parser")
 
-    res = soup.findAll(attrs={'class':'yt-uix-tile-link'})
-    topurl = 'https://www.youtube.com' + res[0]['href']
+    res = soup.findAll(attrs={"class":"yt-uix-tile-link"})
+    topurl = "https://www.youtube.com" + res[0]["href"]
 
     print("====>", textToSearch, topurl)
     subprocess.run('youtube-dl -f bestaudio[asr=44100] --extract-audio --audio-format wav --output "' + textToSearch + '.%(ext)s" --ffmpeg-location ffmpeg-4.3.1-amd64-static/ffmpeg ' + topurl, shell=True, stdout=subprocess.DEVNULL)
@@ -36,29 +36,29 @@ def process(song, performer, year):
 
     # Make vampyhost "RealTime" objects into floats for pickling else they turn to zero
     # (multiprocessing uses pickles for IPC)
-    melody['vector'] = list(melody['vector'])
-    melody['vector'][0] = float(melody['vector'][0])
-    for subdict in chords['list']:
-        subdict['timestamp'] = float(subdict['timestamp'])
-    return (song,performer,year, melody['vector'], chords['list'])
+    melody["vector"] = list(melody["vector"])
+    melody["vector"][0] = float(melody["vector"][0])
+    for subdict in chords["list"]:
+        subdict["timestamp"] = float(subdict["timestamp"])
+    return (song,performer,year, melody["vector"], chords["list"])
 
 
 def writeout(results):
     song,performer,year, melody, chords = results
-    h5file = h5py.File("dataset.hdf5", "a")
-    group = h5file.create_group(song + " " + performer)
-    group.attrs['song'] = song.encode("ascii")
-    group.attrs['performer'] = performer.encode("ascii")
-    group.attrs['year'] = year
+    h5file = h5py.File("dataset.hdf5", 'a')
+    group = h5file.create_group(song + ' ' + performer)
+    group.attrs["song"] = song.encode("ascii")
+    group.attrs["performer"] = performer.encode("ascii")
+    group.attrs["year"] = year
 
     melody_dset = group.create_dataset("melody", data=melody[1])
-    melody_dset.attrs['step_time'] = melody[0]
+    melody_dset.attrs["step_time"] = melody[0]
 
     chord_timestamps = []
     chord_labels = []
     for chordchange in chords:
-        chord_timestamps.append(chordchange['timestamp'])
-        chord_labels.append(chordchange['label'].encode("ascii"))
+        chord_timestamps.append(chordchange["timestamp"])
+        chord_labels.append(chordchange["label"].encode("ascii"))
 
     chord_timestamps_dset = group.create_dataset("chord_timestamps", data=chord_timestamps)
     chord_labels_dset = group.create_dataset("chord_labels", data=chord_labels)
@@ -67,21 +67,22 @@ def writeout(results):
     print("====>", song+" "+performer, "DONE")
 
 
-h5file = h5py.File("dataset.hdf5", "w")
+h5file = h5py.File("dataset.hdf5", 'w')
 h5file.close()
 
 pool = mp.Pool(mp.cpu_count())
 
 songids = []
 
-with open("hs.csv", "r") as file:
+with open("hs.csv", 'r') as file:
     csv = csv.reader(file, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL, skipinitialspace=True)
     next(csv) # clear first line
     for line in csv:
         url,weekid,week_position,song,performer,songid,instance,previous_week_position,peak_position,weeks_on_chart = tuple(line)
         year = int(weekid[-4:])
-        song = "".join([c for c in song if c in string.ascii_letters or c in string.whitespace or c in string.digits])
-        performer = "".join([c for c in performer if c in string.ascii_letters or c in string.whitespace or c in string.digits])
+        song = ''.join([c for c in song if c in string.ascii_letters or c in string.whitespace or c in string.digits])
+        performer = ''.join([c for c in performer if c in string.ascii_letters or c in string.whitespace or c in string.digits])
+        songid = song+performer
         if not year >= 2000:
             continue
         if not songid in songids:
