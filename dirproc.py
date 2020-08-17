1696c96000f438de5c322523f90382a235e5f79d
Aedan Cullen <aedan@rice.edu>
2020-08-17 11:41:55 -0500

    Add chords with and without vocal

--- dirproc.py
+++ dirproc.py
@@ -18,23 +18,28 @@ def monomix(data):
 
 def process(combname, vocal, other, bass):
 
-    chordmix = np.mean([other, bass], axis=0)
+    chordmix_novocal = np.mean([other, bass], axis=0)
+    chordmix_withvocal = np.mean([vocal, other, bass], axis=0)
 
     print("====>", combname, "VAMP...")
     vocalmelody = vamp.collect(vocal, sr, "mtg-melodia:melodia", parameters={"voicing": 0.2})
     othermelody = vamp.collect(other, sr, "mtg-melodia:melodia", parameters={"voicing": 0.2})
     bassmelody = vamp.collect(bass, sr, "mtg-melodia:melodia", parameters={"voicing": 0.2})
-    chords = vamp.collect(chordmix, sr, "nnls-chroma:chordino")
-    chordnotes = vamp.collect(chordmix, sr, "nnls-chroma:chordino", output="chordnotes")
+    chords_novocal = vamp.collect(chordmix_novocal, sr, "nnls-chroma:chordino")
+    chordnotes_novocal = vamp.collect(chordmix_novocal, sr, "nnls-chroma:chordino", output="chordnotes")
+    chords_withvocal = vamp.collect(chordmix_withvocal, sr, "nnls-chroma:chordino")
+    chordnotes_withvocal = vamp.collect(chordmix_withvocal, sr, "nnls-chroma:chordino", output="chordnotes")
 
     # Make vampyhost "RealTime" objects into floats for pickling else they turn to zero
     # (multiprocessing uses pickles for IPC)
     p_vocalmelody = pickleable_melody(vocalmelody)
     p_othermelody = pickleable_melody(othermelody)
     p_bassmelody = pickleable_melody(bassmelody)
-    p_chords = pickleable_chords(chords)
-    p_chordnotes = pickleable_chordnotes(chordnotes)
-    return (combname, p_vocalmelody, p_othermelody, p_bassmelody, p_chords, p_chordnotes)
+    p_chords_novocal = pickleable_chords(chords_novocal)
+    p_chordnotes_novocal = pickleable_chordnotes(chordnotes_novocal)
+    p_chords_withvocal = pickleable_chords(chords_withvocal)
+    p_chordnotes_withvocal = pickleable_chordnotes(chordnotes_withvocal)
+    return (combname, p_vocalmelody, p_othermelody, p_bassmelody, p_chords_novocal, p_chordnotes_novocal, p_chords_withvocal, p_chordnotes_withvocal)
 
 def pickleable_melody(melody):
     melody["vector"] = list(melody["vector"])
@@ -52,8 +57,29 @@ def pickleable_chordnotes(chordnotes):
         subdict["duration"] = float(subdict["duration"])
     return chordnotes["list"]
 
+def push_chord_datasets(group, prefix, chords, chordnotes):
+    chord_timestamps = []
+    chord_labels = []
+    for chordchange in chords:
+        chord_timestamps.append(chordchange["timestamp"])
+        chord_labels.append(chordchange["label"].encode("ascii"))
+
+    chord_timestamps_dset = group.create_dataset(prefix + "_chord_timestamps", data=chord_timestamps)
+    chord_labels_dset = group.create_dataset(prefix + "_chord_labels", data=chord_labels)
+
+
+    chordnote_timestamps = []
+    chordnote_values = []
+    for chordnote in chordnotes:
+        chordnote_timestamps.append(chordnote["timestamp"])
+        chordnote_values.append(chordnote["values"][0])
+
+    chordnote_timestamps_dset = group.create_dataset(prefix + "_chordnote_timestamps", data=chordnote_timestamps)
+    chordnote_values_dset = group.create_dataset(prefix + "_chordnote_values", data=chordnote_values)
+
+
 def writeout(results):
-    combname, vocalmelody, othermelody, bassmelody, chords, chordnotes = results
+    combname, vocalmelody, othermelody, bassmelody, chords_novocal, chordnotes_novocal, chords_withvocal, chordnotes_withvocal = results
     h5file = h5py.File("dirproc_dataset.hdf5", 'a')
     group = h5file.create_group(combname)
 
@@ -64,25 +90,8 @@ def writeout(results):
     bassmelody_dset = group.create_dataset("bassmelody", data=bassmelody[1])
     bassmelody_dset.attrs["step"] = bassmelody[0]
 
-    chord_timestamps = []
-    chord_labels = []
-    for chordchange in chords:
-        chord_timestamps.append(chordchange["timestamp"])
-        chord_labels.append(chordchange["label"].encode("ascii"))
-
-    chord_timestamps_dset = group.create_dataset("chord_timestamps", data=chord_timestamps)
-    chord_labels_dset = group.create_dataset("chord_labels", data=chord_labels)
-
-
-    chordnote_timestamps = []
-    chordnote_values = []
-    for chordnote in chordnotes:
-        chordnote_timestamps.append(chordnote["timestamp"])
-        chordnote_values.append(chordnote["values"][0])
-
-    chordnote_timestamps_dset = group.create_dataset("chordnote_timestamps", data=chordnote_timestamps)
-    chordnote_values_dset = group.create_dataset("chordnote_values", data=chordnote_values)
-
+    push_chord_datasets(group, "novocal", chords_novocal, chordnotes_novocal)
+    push_chord_datasets(group, "withvocal", chords_withvocal, chordnotes_withvocal)
 
     h5file.close()
     print("====>", combname, "DONE")
