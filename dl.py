137c8dd40d1a270d6de64ec565c302b58eccdea7
Aedan Cullen <aedan@pig.vision>
2021-03-28 01:12:42 -0500

    Update dl logic

--- dl.py
+++ dl.py
@@ -1,23 +1,24 @@
 import string
 import csv
 import requests
-from bs4 import BeautifulSoup
 import subprocess
 
-def process(song, performer, year):
-    headers = {"User-Agent": "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"}
+url_prefix = "/watch?v="
+s = requests.Session()
 
-    textToSearch = song + ' ' + performer
+def process(textToSearch):
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"}
+
     url = "https://www.youtube.com/results"
 
-    response = requests.get(url, params={"search_query": textToSearch}, headers=headers)
-    soup = BeautifulSoup(response.content, "html.parser")
+    response = s.get(url, params={"search_query": textToSearch}, headers=headers).text
+    idx = response.find(url_prefix)
+    video_id = response[idx + len(url_prefix) : idx + len(url_prefix) + 11]
 
-    res = soup.findAll(attrs={"class":"yt-uix-tile-link"})
-    topurl = "https://www.youtube.com" + res[0]["href"]
+    topurl = "https://www.youtube.com" + url_prefix + video_id
 
     print("====>", textToSearch, topurl)
-    subprocess.run('youtube-dl -f bestaudio[asr=44100] --extract-audio --audio-format mp3 --output "dl/' + textToSearch + '.%(ext)s" --ffmpeg-location ffmpeg-4.3.1-amd64-static/ffmpeg ' + topurl, shell=True, stdout=subprocess.DEVNULL)
+    subprocess.run('youtube-dl -f bestaudio[asr=44100] --extract-audio --audio-format mp3 --output "dlrandom/' + textToSearch + '.%(ext)s" --ffmpeg-location ffmpeg-4.3.1-amd64-static/ffmpeg ' + topurl, shell=True, stdout=subprocess.DEVNULL)
 
 
 songids = []
@@ -35,6 +36,6 @@ with open("hs.csv", 'r') as file:
             continue
         if not songid in songids:
             songids.append(songid)
-            process(song, performer, year)
+            process(song + ' ' + performer)
 
 print(len(songids), "total")
